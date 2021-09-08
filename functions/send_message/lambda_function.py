16e3c894797b3179a90086cbde467b80490cbc55
Saxon Hunt <shunt@twilio.com>
2021-09-08 16:24:18 -0700

    undo changes to send_message

--- functions/send_message/lambda_function.py
+++ functions/send_message/lambda_function.py
@@ -9,8 +9,6 @@ def handle_state(
     target_type: str,
     as_user: bool = True,
     token: str = "",
-    is_a_file: bool = False,
-    filename: str = "",
 ):
     """Send a Slack message without expecting a response.
     Args:
@@ -27,24 +25,11 @@ def handle_state(
             "Incomplete parameters supplied. Please supply target, target_type and message_template"
         )
 
-    if is_a_file:
-        file_content = socless_template_string(message_template, context)
-        resp = helper.slack_post_msg_wrapper(
-            target,
-            target_type,
-            is_a_file,
-            content=file_content,
-            as_user=as_user,
-            filename=filename,
-        )
+    message = socless_template_string(message_template, context)
 
-    else:
-        # target_id = resolve_slack_target(target, target_type)
-        message = socless_template_string(message_template, context)
-
-        resp = helper.slack_post_msg_wrapper(
-            target, target_type, is_a_file, text=message, as_user=as_user
-        )
+    resp = helper.slack_post_msg_wrapper(
+        target, target_type, text=message, as_user=as_user
+    )
 
     return {"response": resp.data, "slack_id": resp["channel"]}  # type: ignore
 
